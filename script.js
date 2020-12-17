d05f4218cf8a32ede6f51687d3332f54fdfa0c52
utkarshA-optimus <utkarsh.awasthi@optimusinfo.com>
2020-12-18 00:45:20 +0530

    updated proxy server

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function removeLoadingSpinner() {
 // GET QUOTE FROM API
 async function getQuote() {
     showLoadingSpinner();
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyUrl = 'https://agile-anchorage-50476.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
     try {
